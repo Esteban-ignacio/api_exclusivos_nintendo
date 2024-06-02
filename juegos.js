4cd4171098e0aec6ec6ec86b61d655e84985165c
Esteban <es.alamos@duocuc.cl>
2024-06-02 19:12:31 -0400

    juegos exclusivos de la nintendo

---
+++ juegos.js
@@ -0,0 +1,63 @@
+
+
+const juegos = [
+    {
+        "id": "1",
+        "name": "Mario vs Donkey Kong",
+        "img": "https://juegosdigitaleschile.com/files/images/productos/1695249731-mario-vs-donkey-kong-nintendo-switch-0.jpg",
+        "consola": "Nintendo Switch",
+        "precio": "10.000",
+        "cantidad": 20,
+        "descripcion": "Es una serie de juegos de rompecabezas y acción que ofrece una experiencia divertida y desafiante para los fanáticos de Mario y los juegos de puzzles. Con su ingenioso diseño de niveles, mecánica de juego única y encantador estilo visual, ha ganado un lugar especial en el corazón de muchos jugadores."
+    },   
+    {
+        "id": "2",
+        "name": "Digimon Survive",
+        "img": "https://juegosdigitaleschile.com/files/images/productos/1668189960-digimon-survive-nintendo-switch-0.jpg",
+        "consola": "Nintendo Switch",
+        "precio": "6.000",
+        "cantidad": 10,
+        "descripcion": "Juego de rol táctico que promete ofrecer una experiencia emocionante y llena de decisiones en el universo de Digimon"
+    },
+    {
+        "id": "3",
+        "name": "Minecraft",
+        "img": "https://juegosdigitaleschile.com/files/images/productos/1638916284-minecraft-nintendo-switch.jpg",
+        "consola": "Nintendo Switch",
+        "precio": "4.000",
+        "cantidad": 18,
+        "descripcion": "Con su mundo abierto, su libertad creativa y su comunidad activa, sigue siendo uno de los juegos más populares y queridos de la industria del videojuego, y continúa evolucionando y creciendo con el tiempo."
+    },
+    {
+        "id": "4",
+        "name": "Crash Team Racing Nitrio Fueled",
+        "img": "https://juegosdigitaleschile.com/files/images/productos/1637886691-crash-team-racing-nitro-fueled-nintendo-switch.jpg",
+        "consola": "Nintendo Switch",
+        "precio": "10.000",
+        "cantidad": 23,
+        "descripcion": "Es un remake exitoso y fiel del clásico juego de carreras de Crash Bandicoot, que ofrece una experiencia de juego emocionante y competitiva tanto para los fanáticos de toda la vida como para los nuevos jugadores. Con su amplia variedad de personajes, circuitos y opciones de personalización, es un título que sigue siendo apreciado por los amantes de los juegos de carreras y la franquicia Crash Bandicoot."
+    },
+    {
+        "id": "5",
+        "name": "Rayman Legends",
+        "img": "https://juegosdigitaleschile.com/files/images/productos/1637860441-rayman-legends-definitive-edition.jpg",
+        "consola": "Nintendo Switch",
+        "precio": "20.000",
+        "cantidad": 32,
+        "descripcion": "Es un juego de plataformas colorido y divertido que ofrece una experiencia de juego emocionante y desafiante para jugadores de todas las edades. Con su estilo visual único, jugabilidad sólida y una amplia variedad de contenido, es un título que sigue siendo apreciado por los fanáticos de los juegos de plataformas y de la franquicia Rayman."
+    },
+    {
+        "id": "6",
+        "name": "World War Z",
+        "img": "https://juegosdigitaleschile.com/files/images/productos/1642549378-world-war-z-nintendo-switch.jpg",
+        "consola": "Nintendo Switch",
+        "precio": "18.000",
+        "cantidad": 12,
+        "descripcion": "Ofrece una experiencia de juego cooperativa intensa y emocionante, donde los jugadores deben trabajar juntos para sobrevivir a las hordas de zombis en escenarios de todo el mundo. Con su acción frenética, variedad de clases y habilidades, y la amenaza constante de los zombis, el juego proporciona una experiencia divertida y desafiante para los amantes de los juegos de disparos y supervivencia cooperativa."
+    },
+
+];
+
+module.exports = {
+    juegos
+} 
